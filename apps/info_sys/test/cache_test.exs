fe9115b155726cc360c4d246f53ddae1a6152d9c
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-07-18 16:17:12 -0300

    add test unfound and key value pair.

--- apps/info_sys/test/cache_test.exs
+++ apps/info_sys/test/cache_test.exs
@@ -1,5 +1,4 @@
 defmodule InfoSysTest.CacheTest do
-
   use ExUnit.Case, async: true
 
   alias InfoSys.Cache
@@ -7,8 +6,19 @@ defmodule InfoSysTest.CacheTest do
   @moduletag clear_interval: 100
 
   setup %{test: name, clear_interval: clear_interval} do
-    {:ok, pid} Cache.start_link(name: name, clear_interval: clear_interval)
+    {:ok, pid} = Cache.start_link(name: name, clear_interval: clear_interval)
     {:ok, name: name, pid: pid}
   end
 
+  test "key value pair can be put and fetched from cache", %{name: name} do
+    assert :ok = Cache.put(name, :key1, :value1)
+    assert :ok = Cache.put(name, :key2, :value2)
+
+    assert Cache.fetch(name, :key1) == {:ok, :value1}
+    assert Cache.fetch(name, :key2) == {:ok, :value2}
+  end
+
+  test "unfound entry returns errors", %{name: name} do
+    assert Cache.fetch(name, :notexists) == :error
+  end
 end
